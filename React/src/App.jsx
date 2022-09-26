e6099f8d23e41ed463a18eb580342037e07cbfd1
artemiy.dmitrochenko <artemiy.dmitrochenko@devexpress.com>
2022-09-26 13:56:19 +0400

    renamed default assignees

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -12,7 +12,7 @@ const groups = ['assigneeId'];
 const views = ['day'];
 
 const currentDate = new Date('2021-04-26T10:00:00.000Z')
-const defaultAssignees = assignees.map(item => item.id);
+const defaultSelectedAssignees = assignees.map(item => item.id);
 
 const App = () => {
     const [resources, setResources] = useState([
@@ -50,7 +50,7 @@ const App = () => {
             <TagBox
                 className='resources'
                 dataSource={assignees}
-                defaultValue={defaultAssignees}
+                defaultValue={defaultSelectedAssignees}
                 valueExpr='id'
                 displayExpr='text'
                 searchEnabled={true}
